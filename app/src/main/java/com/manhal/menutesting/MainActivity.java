02377cd6d1f7639f5e982941b29fd9a11446ac19
scorpiomenhal <m.alaskar@live.com>
2021-09-07 21:08:55 -0400

    Initial commit

---
+++ app/src/main/java/com/manhal/menutesting/MainActivity.java
@@ -0,0 +1,30 @@
+package com.manhal.menutesting;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+        @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+}
